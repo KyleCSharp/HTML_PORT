8e14f02b6b74a7fff2fa4c77557854268c915baa
Kyle Rigler <kylelrigler@gmail.com>
2023-07-21 12:39:03 -0400

    added loading wheel

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,4 @@
-/* document.addEventListener("click", (event) => {
-    const contactButton = document.getElementById("contactButton");
-    const popupOverlay = document.getElementById("popupOverlay");
-    const closeButton = document.getElementById("closeButton");
 
-    if (event.target === contactButton) {
-      popupOverlay.classList.add("show-popup");
-    }
-
-    if (event.target === closeButton) {
-      popupOverlay.classList.remove("show-popup");
-    }
-  }); */
   document.addEventListener("click", (event) => {
     const contactButton = document.querySelector(".contact-button"); 
     const popupOverlay = document.getElementById("popupOverlay");
@@ -24,3 +12,14 @@
       popupOverlay.classList.remove("show-popup"); 
     }
   });
+  function submitForm() {
+    // Show the spinner
+    document.getElementById('spinner').style.display = 'block';
+    
+    
+    // Disable the submit button to prevent multiple clicks during form submission
+    document.getElementById('submitBtn').disabled = true;
+    
+    // Submit the form programmatically
+    document.getElementById('myForm').submit();
+}
